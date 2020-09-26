ad960cac894800f7d23521c6c9dfa43eff054cac
Matt Schafer <lucidsigma17@gmail.com>
2020-09-26 20:39:27 +1000

    Replace OpenAL with SoLoud.

--- Stardust/Stardust/src/Main.cpp
+++ Stardust/Stardust/src/Main.cpp
@@ -3,8 +3,32 @@
 #include "sandbox/TestScene.h"
 #include "stardust/Stardust.h"
 
+#include <iostream>
+#include <memory>
+#include <soloud/soloud.h>
+#include <soloud/soloud_wavstream.h>
+
 int main([[maybe_unused]] const int argc, char* argv[])
 {
+	std::unique_ptr<SoLoud::Soloud> soundSystem = std::make_unique<SoLoud::Soloud>();
+	soundSystem->init();
+
+	SoLoud::WavStream sample;
+	sample.load("test.wav");
+
+	auto handle = soundSystem->play(sample, 1.0f, 0.0f, true);
+
+	std::cout << std::boolalpha << (soundSystem->isValidVoiceHandle(handle) && !soundSystem->getPause(handle)) << "\n";
+	soundSystem->setPause(handle, false);
+	std::cout << std::boolalpha << (soundSystem->isValidVoiceHandle(handle) && !soundSystem->getPause(handle)) << "\n";
+
+	while (soundSystem->isValidVoiceHandle(handle))
+	{
+		;
+	}
+
+	soundSystem->deinit();
+
 	const stardust::Application::FilepathsInfo gameFilepaths{
 		.windowIcon = "assets/icon/icon.png",
 		.config = "config/config.json",
